abeacebb4848cb34829bec964a4b6009461f3ef0
mohammadfayaj <mohammadfayaj36@gmail.com>
2021-04-12 12:52:17 +0600

    init

--- donatory/models.py
+++ donatory/models.py
@@ -35,12 +35,13 @@ class DonorInformation(models.Model):
 
     def save(self, *args, **kwargs):
         super().save()
-        img = Image.open(self.profile_picture.path)
+        if self.profile_picture:
+            img = Image.open(self.profile_picture.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (100, 100)
-            img.thumbnail(output_size)
-            img.save(self.profile_picture.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (100, 100)
+                img.thumbnail(output_size)
+                img.save(self.profile_picture.path)
 
 
 
--- donatory/views.py
+++ donatory/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,redirect,get_object_or_404,HttpResponseRedir
 from donatory.models import DonorInformation
 from donatory.forms import DonorInformationForm
 from .filters import DonorInformationFilter
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 def donatory_home(request):
     template = 'donatory/donatory_home.html'
@@ -10,9 +11,10 @@ def donatory_home(request):
     filter = DonorInformationFilter(request.GET, queryset=donatory_qs)
     return render(request, template, {'filter': filter,})
 
+@login_required
 def donor_blood_form_edit(request,id):
     template = 'donatory/edit.html'
-    qs = get_object_or_404(DonorInformation, id=id)
+    qs = get_object_or_404(DonorInformation, id=id, user=request.user)
     if request.method == 'POST':
         form = DonorInformationForm(request.POST, request.FILES, instance=qs)
         if form.is_valid():
@@ -23,25 +25,33 @@ def donor_blood_form_edit(request,id):
         form = DonorInformationForm(instance=qs)
     return render(request, template, {'form': form, 'qs':qs })
 
+    
+@login_required
 def donor_blood_form_delete(request,id):
     template = 'donatory/confirm_delete.html'
-    qs = get_object_or_404(DonorInformation, id=id)
+    qs = get_object_or_404(DonorInformation, id=id, user=request.user)
     if request.method == "POST":
         qs.delete()
         next = request.POST.get('next', '/')
         return HttpResponseRedirect(next)
     return render(request, template, {'qs': qs})
 
+
+@login_required
 def blood_form(request):
     template = 'donatory/blood_form.html'
 
     if request.method == 'POST':
         form = DonorInformationForm(request.POST, request.FILES)
         if form.is_valid():
+            user = form.cleaned_data.get('user')
+            form = form.save(commit=False) # not yet save in database
+            form.user = request.user # select user
             form.save()
             return redirect('donatory:home')
     else:
         form = DonorInformationForm()
+        
 
     context = {'form': form,}
     return render(request, template, context)
